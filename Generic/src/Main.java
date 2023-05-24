c136105e97e14db7dea203f7232d634a14ad0324
fatihclsknn <faatihcaliskan@gmail.com>
2023-05-24 09:28:54 +0300

    generic silindi

--- Generic/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        MyList<Integer> liste = new MyList<>(10);
-        System.out.println("Dizideki Eleman Sayısı : " + liste.size());
-
-    }
-}
